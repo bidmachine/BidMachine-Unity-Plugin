caee781fb0859ee9f86a5593099912598e7fd7ca
KulArtsiom <kulartsiom@gmail.com>
2021-08-30 10:38:52 +0300

    Update BidMachineSettings.cs

--- Assets/BidMachine/Editor/BidMachineSettings.cs
+++ Assets/BidMachine/Editor/BidMachineSettings.cs
@@ -1,26 +1,13 @@
 #if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
-using BidMachineAds.Unity.Common;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-[InitializeOnLoad]
 public class BidMachineSettings : ScriptableObject
 {
 	[MenuItem("BidMachine/SDK Documentation")]
 	public static void OpenDocumentation()
 	{
-		string url = "https://wiki.appodeal.com/display/BID/BidMachine";
-		Application.OpenURL(url);
+		Application.OpenURL("https://docs.bidmachine.io/docs");
 	}
-	
-	[MenuItem("BidMachine/Appodeal Homepage")]
-	public static void OpenAppodealHome()
-	{
-		string url = "http://www.appodeal.com";
-		Application.OpenURL(url);
-	}
-
 }
 #endif
