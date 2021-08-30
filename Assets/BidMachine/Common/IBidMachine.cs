ba989af1f1f68a17e3d1ef9ea40b02b76e2023b9
KulArtsiom <kulartsiom@gmail.com>
2021-08-30 15:36:22 +0300

    Update IBidMachine.cs

--- Assets/BidMachine/Common/IBidMachine.cs
+++ Assets/BidMachine/Common/IBidMachine.cs
@@ -1,37 +1,60 @@
-﻿using BidMachineAds.Unity.Android;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using BidMachineAds.Unity.Android;
 using BidMachineAds.Unity.Api;
 using UnityEngine;
 
 namespace BidMachineAds.Unity.Common
 {
-	public interface IBidMachine
-	{
-		void initialize(string sallerId);
-		void setLoggingEnabled(bool logging);
-		void setTestMode(bool test);
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public interface IBidMachine
+    {
+        void initialize(string sellerId);
+        bool isInitialized();
+        void setEndpoint(string url);
+        void setLoggingEnabled(bool logging);
+        void setTestMode(bool test);
         void setTargetingParams(TargetingParams targetingParams);
         void setConsentConfig(bool consent, string consentConfig);
         void setSubjectToGDPR(bool subjectToGDPR);
         void setCoppa(bool coppa);
+        void setUSPrivacyString(string usPrivacyString);
+
+        //void setPublisher(Publisher publisher);
+        // void registerNetworks(@ NetworkConfig... networkConfigs);
+        // void registerNetworks(Context context, @NonNull String jsonData);
+        // void registerAdRequestListener(@NonNull AdRequest.AdRequestListener adRequestListener);
+        // void unregisterAdRequestListener(@NonNull AdRequest.AdRequestListener adRequestListener);
         bool checkAndroidPermissions(string permission);
         void requestAndroidPermissions();
     }
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public interface ITargetingParams
     {
         void setUserId(string id);
         void setGender(TargetingParams.Gender gender);
         void setBirthdayYear(int year);
         void setKeyWords(string[] keyWords);
-        void setDeviceLocation(double latitude, double longitude);
         void setCountry(string country);
         void setCity(string city);
         void setZip(string zip);
         void setStoreUrl(string storeUrl);
-        void setPaid(bool paid);
-        void setBlockedAdvertiserIABCategories(string categories);
-        void setBlockedAdvertiserDomain(string domains);
-        void setBlockedApplication(string applications);
+        void setStoreCategory(string storeCategory);
+        void setStoreSubCategories(string storeSubCategories);
+        void setFramework(string framework);
+        void setPaid(bool paid); 
+        void setDeviceLocation(string providerName, double latitude, double longitude);
+        void setExternalUserIds(ExternalUserId[] externalUserIdList);
+        void addBlockedApplication(string bundleOrPackage);
+        void addBlockedAdvertiserIABCategory(string category);
+        void addBlockedAdvertiserDomain(string domain);
+    }
+
+    public interface IExternalUserId
+    {
+        
     }
 
     public interface IPriceFloorParams
@@ -63,9 +86,17 @@ namespace BidMachineAds.Unity.Common
         IRewardedRequest build();
     }
 
-    public interface IBannerRequest{ }
-    public interface IInterstitialRequest { }
-    public interface IRewardedRequest { }
+    public interface IBannerRequest
+    {
+    }
+
+    public interface IInterstitialRequest
+    {
+    }
+
+    public interface IRewardedRequest
+    {
+    }
 
     public interface IBannerView
     {
@@ -98,5 +129,4 @@ namespace BidMachineAds.Unity.Common
         void setListener(IRewardedAdListener rewardedAdListener);
         void load(RewardedRequest rewardedRequest);
     }
-
 }
