6cb8bc51d78776e5e6ddbdd01f52d3124be89560
KulArtsiom <kulartsiom@gmail.com>
2021-08-30 11:26:52 +0300

    Update BidMachineDemoController.cs

--- Assets/BidMachine/Demo/BidMachineDemoController.cs
+++ Assets/BidMachine/Demo/BidMachineDemoController.cs
@@ -1,9 +1,15 @@
+using System;
 using UnityEngine;
 using BidMachineAds.Unity.Api;
 using BidMachineAds.Unity.Common;
+using UnityEngine.UI;
 
 public class BidMachineDemoController : MonoBehaviour, IInterstitialAdListener, IRewardedAdListener, IBannerListener
 {
+    
+    [SerializeField] public Toggle tgTesting;
+    [SerializeField] public Toggle tgLogging;
+    
     TargetingParams targetingParams;
     PriceFloorParams priceFloorParams;
 
@@ -19,79 +25,85 @@ public class BidMachineDemoController : MonoBehaviour, IInterstitialAdListener, 
     BannerRequestBuilder bannerRequestBuilder;
     BannerView bannerView;
     Banner banner;
-    
+
+    private void Start()
+    {
+        tgTesting.isOn = true;
+        tgLogging.isOn = true;
+    }
+
     public void BidMachineInitialize()
     {
-        targetingParams = new TargetingParams();
-        priceFloorParams = new PriceFloorParams();
-
-        /* Vendor-specific ID for the user. */
-        targetingParams.setUserId("1");
-
-        /* Gender, one of following: Female, Male, Omitted. */
-        targetingParams.setGender(TargetingParams.Gender.Male);
-
-        /* Year of birth as a 4-digit integer (e.g - 1990). */
-        targetingParams.setBirthdayYear(1990);
-
-        /* List of keywords, interests, or intents (separated by comma if you use .xml). */
-        targetingParams.setKeyWords(new[] { "games", "sport" });
-
-        /* Location of the device. It might not be the location that was sent to the server,
-         * as it is compared with the current device location at the time, when it was received. */
-        targetingParams.setDeviceLocation(12.8888, 12.222);
-
-        /* Country of the user's home base (i.e., not necessarily their current location). */
-        targetingParams.setCountry("USA");
-
-        /*City of the user's home base (i.e., not necessarily their current location).*/
-        targetingParams.setCity("New-York");
-
-        /* Zip of the user's home base (i.e., not necessarily their current location). */
-        targetingParams.setZip("10001");
-
-        /* App store URL for an installed app; for IQG 2.1 compliance. */
-        targetingParams.setStoreUrl("https://test.com");
-
-        /* Determines, if it is a free or paid version of the app. */
-        targetingParams.setPaid(true);
-
-        /* Block list of content categories using IDs. */
-        targetingParams.setBlockedAdvertiserIABCategories("IAB26,AB25");
-
-        /* Block list of advertisers by their domains (e.g., “example.com”). */
-        targetingParams.setBlockedAdvertiserDomain("example.com");
-
-        /* Block list of apps where ads are disallowed. These should be bundle or package names
-         * (e.g., “com.foo.mygame”) and should NOT be app store IDs (e.g., not iTunes store IDs). */
-        targetingParams.setBlockedApplication("com.foo.mygame");
-
-        priceFloorParams = new PriceFloorParams();
-        priceFloorParams.setPriceFloor("sample", 2.15);
+        // targetingParams = new TargetingParams();
+        // priceFloorParams = new PriceFloorParams();
+        //
+        // /* Vendor-specific ID for the user. */
+        // targetingParams.setUserId("1");
+        //
+        // /* Gender, one of following: Female, Male, Omitted. */
+        // targetingParams.setGender(TargetingParams.Gender.Male);
+        //
+        // /* Year of birth as a 4-digit integer (e.g - 1990). */
+        // targetingParams.setBirthdayYear(1990);
+        //
+        // /* List of keywords, interests, or intents (separated by comma if you use .xml). */
+        // targetingParams.setKeyWords(new[] { "games", "sport" });
+        //
+        // /* Location of the device. It might not be the location that was sent to the server,
+        //  * as it is compared with the current device location at the time, when it was received. */
+        // targetingParams.setDeviceLocation(12.8888, 12.222);
+        //
+        // /* Country of the user's home base (i.e., not necessarily their current location). */
+        // targetingParams.setCountry("USA");
+        //
+        // /*City of the user's home base (i.e., not necessarily their current location).*/
+        // targetingParams.setCity("New-York");
+        //
+        // /* Zip of the user's home base (i.e., not necessarily their current location). */
+        // targetingParams.setZip("10001");
+        //
+        // /* App store URL for an installed app; for IQG 2.1 compliance. */
+        // targetingParams.setStoreUrl("https://test.com");
+        //
+        // /* Determines, if it is a free or paid version of the app. */
+        // targetingParams.setPaid(true);
+        //
+        // /* Block list of content categories using IDs. */
+        // targetingParams.setBlockedAdvertiserIABCategories("IAB26,AB25");
+        //
+        // /* Block list of advertisers by their domains (e.g., “example.com”). */
+        // targetingParams.setBlockedAdvertiserDomain("example.com");
+        //
+        // /* Block list of apps where ads are disallowed. These should be bundle or package names
+        //  * (e.g., “com.foo.mygame”) and should NOT be app store IDs (e.g., not iTunes store IDs). */
+        // targetingParams.setBlockedApplication("com.foo.mygame");
+        //
+        // priceFloorParams = new PriceFloorParams();
+        // priceFloorParams.setPriceFloor("sample", 2.15);
 
         /* Enable logs */
-        BidMachine.setLoggingEnabled(true);
+        BidMachine.setLoggingEnabled(tgLogging.isOn);
 
         /* Enable test mode */
-        BidMachine.setTestMode(true);
+        BidMachine.setTestMode(tgTesting.isOn);
 
         /* Initialize BidMachine */
         BidMachine.initialize("1");
 
-        /* Set default Targeting params */
-        BidMachine.setTargetingParams(targetingParams);
-
-        /* Set consent config. GDPR consent string( if applicable), indicating the compliance to the IAB standard Consent String Format
-         * of the Transparency and Consent Framework technical specifications. */
-        BidMachine.setConsentConfig(true, "Something");
-
-        /* Set Subject to GDPR. Flag indicating if GDPR regulations can be applied.
-         * The General Data Protection Regulation (GDPR) is a regulation of the European Union.*/
-        BidMachine.setSubjectToGDPR(true);
-
-        /* Set Coppa. Flag indicating if COPPA regulations can be applied.
-         * The Children's Online Privacy Protection Act (COPPA) was established by the U.S. Federal Trade Commission. */
-        BidMachine.setCoppa(true);
+        // /* Set default Targeting params */
+        // BidMachine.setTargetingParams(targetingParams);
+        //
+        // /* Set consent config. GDPR consent string( if applicable), indicating the compliance to the IAB standard Consent String Format
+        //  * of the Transparency and Consent Framework technical specifications. */
+        // BidMachine.setConsentConfig(true, "Something");
+        //
+        // /* Set Subject to GDPR. Flag indicating if GDPR regulations can be applied.
+        //  * The General Data Protection Regulation (GDPR) is a regulation of the European Union.*/
+        // BidMachine.setSubjectToGDPR(true);
+        //
+        // /* Set Coppa. Flag indicating if COPPA regulations can be applied.
+        //  * The Children's Online Privacy Protection Act (COPPA) was established by the U.S. Federal Trade Commission. */
+        // BidMachine.setCoppa(true);
     }
 
     public void LoadInterstitialAd()
