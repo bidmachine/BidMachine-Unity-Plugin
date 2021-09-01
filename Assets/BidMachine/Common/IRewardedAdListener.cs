3b109fa8dfcb955177a54b8e0b4c5160e0201bbb
KulArtsiom <kulartsiom@gmail.com>
2021-09-01 17:11:14 +0300

    Update IRewardedAdListener.cs

--- Assets/BidMachine/Common/IRewardedAdListener.cs
+++ Assets/BidMachine/Common/IRewardedAdListener.cs
@@ -1,59 +1,61 @@
-﻿using BidMachineAds.Unity.Api;
+﻿using System.Diagnostics.CodeAnalysis;
+using BidMachineAds.Unity.Api;
 using BidMachineAds.Unity.Android;
 
 namespace BidMachineAds.Unity.Common
 {
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public interface IRewardedAdListener
     {
         /**
         * Called when Ad was loaded and ready to be shown
         */
-        void onAdLoaded(RewardedAd ad);
+        void onRewardedAdLoaded(RewardedAd ad);
 
         /**
         * Called when Ad load failed
         */
-        void onAdLoadFailed(RewardedAd ad, BMError error);
+        void onRewardedAdLoadFailed(RewardedAd ad, BMError error);
 
         /**
         * Called when Ad shown
         */
-        void onAdShown(RewardedAd ad);
+        void onRewardedAdShown(RewardedAd ad);
 
         /**
         * Called when Ad Impression was tracked
         */
-        void onAdImpression(RewardedAd ad);
+        void onRewardedAdImpression(RewardedAd ad);
 
         /**
         * Called when Ad was clicked
         */
-        void onAdClosed(RewardedAd ad);
+        void onRewardedAdClosed(RewardedAd ad);
 
         /**
         * Called when Ad was clicked
         */
-        void onAdClicked(RewardedAd ad);
+        void onRewardedAdClicked(RewardedAd ad);
 
         /**
         * Called when Ad expired
         */
-        void onAdExpired(RewardedAd ad);
+        void onRewardedAdExpired(RewardedAd ad);
 
         /**
         * Called when ad show failed
         */
-        void onAdShowFailed(RewardedAd ad, BMError error);
+        void onRewardedAdShowFailed(RewardedAd ad, BMError error);
 
         /**
         * Called when ad was closed (e.g - user click close button)
         */
-        void onAdClosed(RewardedAd ad, bool finished);
+        void onRewardedAdClosed(RewardedAd ad, bool finished);
 
         /**
         * Called when Rewarded Ad Complete (e.g - the video has been played to the end).
         * You can use this event to initialize your reward
         */
-        void onAdRewarded(RewardedAd ad);
+        void onRewardedAdRewarded(RewardedAd ad);
     }
 }
