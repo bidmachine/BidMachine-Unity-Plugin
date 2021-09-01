921d7a9c6efe535fb5dec4fb01c700b822956288
KulArtsiom <kulartsiom@gmail.com>
2021-09-01 14:13:03 +0300

    Update BidMachineDemoController.cs

--- Assets/BidMachine/Demo/BidMachineDemoController.cs
+++ Assets/BidMachine/Demo/BidMachineDemoController.cs
@@ -37,51 +37,51 @@ public class BidMachineDemoController : MonoBehaviour, IInterstitialAdListener, 
 
     public void BidMachineInitialize()
     {
-        // targetingParams = new TargetingParams();
-        // targetingParams.setUserId("1");
-        // targetingParams.setGender(TargetingParams.Gender.Female);
-        // targetingParams.setBirthdayYear(1991);
-        // targetingParams.setKeyWords(new[] { "games, sport" });
-        // targetingParams.setCountry("Belarus");
-        // targetingParams.setCity("Minsk");
-        // targetingParams.setZip("220059");
-        // targetingParams.setStoreUrl("https://store.url");
-        // targetingParams.setStoreCategory("cards");
-        // targetingParams.setStoreSubCategories(new[] { "games", "cards" });
-        // targetingParams.setFramework("unity");
-        // targetingParams.setFramework("unity");
-        // targetingParams.setPaid(true);
-        // targetingParams.setDeviceLocation("", 22.0d, 22.0d);
-        // targetingParams.setExternalUserIds(new[]
-        // {
-        //     new ExternalUserId("sourceId_1", "1"),
-        //     new ExternalUserId("sourceId_2", "2")
-        // });
-        // targetingParams.addBlockedApplication("com.appodeal.test");
-        // targetingParams.addBlockedAdvertiserIABCategory("IAB-71");
-        // targetingParams.addBlockedAdvertiserDomain("ua");
-        //
-        // priceFloorParams = new PriceFloorParams();
-        // priceFloorParams.addPriceFloor("123", 1.2d);
-        // sessionAdParams = new SessionAdParams()
-        //     .setSessionDuration(123)
-        //     .setImpressionCount(123)
-        //     .setClickRate(1.2f)
-        //     .setIsUserClickedOnLastAd(true)
-        //     .setCompletionRate(1.3f);
+        targetingParams = new TargetingParams();
+        targetingParams.setUserId("1");
+        targetingParams.setGender(TargetingParams.Gender.Female);
+        targetingParams.setBirthdayYear(1991);
+        targetingParams.setKeyWords(new[] { "games, sport" });
+        targetingParams.setCountry("Belarus");
+        targetingParams.setCity("Minsk");
+        targetingParams.setZip("220059");
+        targetingParams.setStoreUrl("https://store.url");
+        targetingParams.setStoreCategory("cards");
+        targetingParams.setStoreSubCategories(new[] { "games", "cards" });
+        targetingParams.setFramework("unity");
+        targetingParams.setFramework("unity");
+        targetingParams.setPaid(true);
+        targetingParams.setDeviceLocation("", 22.0d, 22.0d);
+        targetingParams.setExternalUserIds(new[]
+        {
+            new ExternalUserId("sourceId_1", "1"),
+            new ExternalUserId("sourceId_2", "2")
+        });
+        targetingParams.addBlockedApplication("com.appodeal.test");
+        targetingParams.addBlockedAdvertiserIABCategory("IAB-71");
+        targetingParams.addBlockedAdvertiserDomain("ua");
+
+        priceFloorParams = new PriceFloorParams();
+        priceFloorParams.addPriceFloor("123", 1.2d);
+        sessionAdParams = new SessionAdParams()
+            .setSessionDuration(123)
+            .setImpressionCount(123)
+            .setClickRate(1.2f)
+            .setIsUserClickedOnLastAd(true)
+            .setCompletionRate(1.3f);
 
 
-        //  BidMachine.setPublisher(new Publisher("1", "Gena", "ua", new[] { "games, cards" }));
+        BidMachine.setPublisher(new Publisher("1", "Gena", "ua", new[] { "games, cards" }));
         BidMachine.setLoggingEnabled(tgLogging.isOn);
         BidMachine.setTestMode(tgTesting.isOn);
-        //  BidMachine.setEndpoint("https://test.com");
-        //BidMachine.setTargetingParams(targetingParams);
+        BidMachine.setEndpoint("https://test.com");
+        BidMachine.setTargetingParams(targetingParams);
         BidMachine.initialize("1");
-        // BidMachine.setConsentConfig(true, "test consent string");
-        // BidMachine.setSubjectToGDPR(true);
-        // BidMachine.setCoppa(true);
-        // BidMachine.setUSPrivacyString("test_string");
-        // BidMachine.checkAndroidPermissions(Permission.CoarseLocation);
+        BidMachine.setConsentConfig(true, "test consent string");
+        BidMachine.setSubjectToGDPR(true);
+        BidMachine.setCoppa(true);
+        BidMachine.setUSPrivacyString("test_string");
+        BidMachine.checkAndroidPermissions(Permission.CoarseLocation);
     }
 
     public void IsInitialized()
@@ -193,11 +193,13 @@ public class BidMachineDemoController : MonoBehaviour, IInterstitialAdListener, 
     {
         bannerRequest = new BannerRequestBuilder()
             .setSize(BannerSize.Size_320х50)
-            // .setTargetingParams(targetingParams)
-            // .setPriceFloorParams(priceFloorParams)
-            // .setSessionAdParams(sessionAdParams)
-            // .setPlacementId("placement1")
-            // .setLoadingTimeOut(123)
+            .setTargetingParams(targetingParams)
+            .setPriceFloorParams(priceFloorParams)
+            .setSessionAdParams(sessionAdParams)
+            .setPlacementId("placement1")
+            .setLoadingTimeOut(123)
+            .setBidPayload("123")
+            .setNetworks("admob")
             .setListener(this)
             .build();
 
