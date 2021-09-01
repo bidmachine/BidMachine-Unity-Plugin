3a54eb05020450fe40dc70b140927e55039e843d
KulArtsiom <kulartsiom@gmail.com>
2021-09-01 17:11:16 +0300

    Update IInterstitialAdListener.cs

--- Assets/BidMachine/Common/IInterstitialAdListener.cs
+++ Assets/BidMachine/Common/IInterstitialAdListener.cs
@@ -1,52 +1,54 @@
-﻿using BidMachineAds.Unity.Api;
+﻿using System.Diagnostics.CodeAnalysis;
+using BidMachineAds.Unity.Api;
 
 namespace BidMachineAds.Unity.Common
 {
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public interface IInterstitialAdListener
     {
         /**
         * Called when Ad was loaded and ready to be shown
         */
-        void onAdLoaded(InterstitialAd ad);
+        void onInterstitialAdLoaded(InterstitialAd ad);
 
         /**
         * Called when Ad load failed
         */
-        void onAdLoadFailed(InterstitialAd ad, BMError error);
+        void onInterstitialAdLoadFailed(InterstitialAd ad, BMError error);
 
         /**
         * Called when Ad shown
         */
-        void onAdShown(InterstitialAd ad);
+        void onInterstitialAdShown(InterstitialAd ad);
 
         /**
         * Called when Ad Impression was tracked
         */
-        void onAdImpression(InterstitialAd ad);
+        void onInterstitialAdImpression(InterstitialAd ad);
 
         /**
         * Called when Ad was clicked
         */
-        void onAdClicked(InterstitialAd ad);
+        void onInterstitialAdClicked(InterstitialAd ad);
 
         /**
         * Called when Ad was clicked
         */
-        void onAdClosed(InterstitialAd ad);
+        void onInterstitialAdClosed(InterstitialAd ad);
 
         /**
         * Called when Ad expired
         */
-        void onAdExpired(InterstitialAd ad);
+        void onInterstitialAdExpired(InterstitialAd ad);
 
         /**
         * Called when ad show failed
         */
-        void onAdShowFailed(InterstitialAd ad, BMError error);
+        void onInterstitialAdShowFailed(InterstitialAd ad, BMError error);
 
         /**
         * Called when ad was closed (e.g - user click close button)
         */
-        void onAdClosed(InterstitialAd ad, bool finished);
+        void onInterstitialAdClosed(InterstitialAd ad, bool finished);
     }
 }
