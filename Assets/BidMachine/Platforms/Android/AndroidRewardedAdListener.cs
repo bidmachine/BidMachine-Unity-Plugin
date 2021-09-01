99006d57700913fd71d00331fecafc104a2a6b57
KulArtsiom <kulartsiom@gmail.com>
2021-09-01 17:11:09 +0300

    Update AndroidRewardedAdListener.cs

--- Assets/BidMachine/Platforms/Android/AndroidRewardedAdListener.cs
+++ Assets/BidMachine/Platforms/Android/AndroidRewardedAdListener.cs
@@ -22,12 +22,12 @@ namespace BidMachineAds.Unity.Android
 
         void onAdLoaded(AndroidJavaObject ad)
         {
-            listener.onAdLoaded(new RewardedAd(new AndroidRewardedAd(ad)));
+            listener.onRewardedAdLoaded(new RewardedAd(new AndroidRewardedAd(ad)));
         }
 
         void onAdClosed(AndroidJavaObject ad, bool finished)
         {
-            listener.onAdClosed(new RewardedAd(new AndroidRewardedAd(ad)), finished);
+            listener.onRewardedAdClosed(new RewardedAd(new AndroidRewardedAd(ad)), finished);
         }
 
         void onAdLoadFailed(AndroidJavaObject ad, AndroidJavaObject error)
@@ -37,12 +37,12 @@ namespace BidMachineAds.Unity.Android
                 code = error.Call<int>("getCode"),
                 message = error.Call<string>("getMessage")
             };
-            listener.onAdLoadFailed((new RewardedAd(new AndroidRewardedAd(ad))), bmError);
+            listener.onRewardedAdLoadFailed((new RewardedAd(new AndroidRewardedAd(ad))), bmError);
         }
 
         void onAdShown(AndroidJavaObject ad)
         {
-            listener.onAdShown((new RewardedAd(new AndroidRewardedAd(ad))));
+            listener.onRewardedAdShown((new RewardedAd(new AndroidRewardedAd(ad))));
         }
 
         void onAdShowFailed(AndroidJavaObject ad, AndroidJavaObject error)
@@ -52,27 +52,27 @@ namespace BidMachineAds.Unity.Android
                 code = error.Call<int>("getCode"),
                 message = error.Call<string>("getMessage")
             };
-            listener.onAdShowFailed((new RewardedAd(new AndroidRewardedAd(ad))), bmError);
+            listener.onRewardedAdShowFailed((new RewardedAd(new AndroidRewardedAd(ad))), bmError);
         }
 
         void onAdImpression(AndroidJavaObject ad)
         {
-            listener.onAdImpression((new RewardedAd(new AndroidRewardedAd(ad))));
+            listener.onRewardedAdImpression((new RewardedAd(new AndroidRewardedAd(ad))));
         }
 
         void onAdClicked(AndroidJavaObject ad)
         {
-            listener.onAdClicked((new RewardedAd(new AndroidRewardedAd(ad))));
+            listener.onRewardedAdClicked((new RewardedAd(new AndroidRewardedAd(ad))));
         }
 
         void onAdExpired(AndroidJavaObject ad)
         {
-            listener.onAdExpired((new RewardedAd(new AndroidRewardedAd(ad))));
+            listener.onRewardedAdExpired((new RewardedAd(new AndroidRewardedAd(ad))));
         }
 
         void onAdRewarded(AndroidJavaObject ad)
         {
-            listener.onAdRewarded((new RewardedAd(new AndroidRewardedAd(ad))));
+            listener.onRewardedAdRewarded((new RewardedAd(new AndroidRewardedAd(ad))));
         }
     }
 #else
