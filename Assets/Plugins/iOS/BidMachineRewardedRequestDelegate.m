9db836a4d3ef11cb4a00afef26577ddcae869371
KulArtsiom <kulartsiom@gmail.com>
2021-09-07 15:31:51 +0300

    Create BidMachineRewardedRequestDelegate.m

---
+++ Assets/Plugins/iOS/BidMachineRewardedRequestDelegate.m
@@ -0,0 +1,44 @@
+#import "BidMachineRewardedRequestDelegate.h"
+
+@implementation BidMachineRewardedRequestDelegate : NSObject
+
+- (void)request:(BDMRequest *)request failedWithError:(NSError *)error {
+    if (self.onRewardedRequestFailed) {
+        self.onRewardedRequestFailed((BDMRewardedRequest *)request,error);
+    }
+}
+
+- (void)request:(BDMRequest *)request completeWithInfo:(BDMAuctionInfo *)info {
+    
+    NSString *jsonString = @"";
+    NSMutableDictionary *dictionary = [NSMutableDictionary new];
+    dictionary[@"adDomains"] = info.adDomains;
+    dictionary[@"bidID"] = info.bidID;
+    dictionary[@"cID"] = info.cID;
+    dictionary[@"creativeID"] = info.creativeID;
+    dictionary[@"customParams"] = info.customParams;
+    dictionary[@"dealID"] = info.dealID;
+    dictionary[@"demandSource"] = info.demandSource;
+    dictionary[@"price"] = info.price;
+    
+    NSData *data = [NSJSONSerialization dataWithJSONObject:dictionary options:0 error:nil];
+    if (data) {
+        jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    }
+    const char *cString = [jsonString UTF8String];
+    char *cStringCopy = calloc([jsonString length]+1, 1);
+    
+    char infoChar = strncpy(cStringCopy, cString, [jsonString length]);
+    
+    if (self.onRewardedRequestSuccess) {
+        self.onRewardedRequestSuccess((BDMRewardedRequest *)request, infoChar);
+    }
+}
+
+- (void)requestDidExpire:(BDMRequest *)request {
+    if (self.onRewardedRequestExpired) {
+        self.onRewardedRequestExpired((BDMRewardedRequest *)request);
+    }
+}
+
+@end
