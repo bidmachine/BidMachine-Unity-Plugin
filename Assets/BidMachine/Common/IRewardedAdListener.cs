8e1d712596c424ee22cff6717672982ae1730ef6
KulArtsiom <kulartsiom@gmail.com>
2021-09-17 10:51:03 +0300

    Update IRewardedAdListener.cs

--- Assets/BidMachine/Common/IRewardedAdListener.cs
+++ Assets/BidMachine/Common/IRewardedAdListener.cs
@@ -7,55 +7,15 @@ namespace BidMachineAds.Unity.Common
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public interface IRewardedAdListener
     {
-        /**
-        * Called when Ad was loaded and ready to be shown
-        */
+        
         void onRewardedAdLoaded(RewardedAd ad);
-
-        /**
-        * Called when Ad load failed
-        */
         void onRewardedAdLoadFailed(RewardedAd ad, BMError error);
-
-        /**
-        * Called when Ad shown
-        */
         void onRewardedAdShown(RewardedAd ad);
-
-        /**
-        * Called when Ad Impression was tracked
-        */
-        void onRewardedAdImpression(RewardedAd ad);
-
-        /**
-        * Called when Ad was clicked
-        */
-        void onRewardedAdClosed(RewardedAd ad);
-
-        /**
-        * Called when Ad was clicked
-        */
-        void onRewardedAdClicked(RewardedAd ad);
-
-        /**
-        * Called when Ad expired
-        */
-        void onRewardedAdExpired(RewardedAd ad);
-
-        /**
-        * Called when ad show failed
-        */
         void onRewardedAdShowFailed(RewardedAd ad, BMError error);
-
-        /**
-        * Called when ad was closed (e.g - user click close button)
-        */
-        void onRewardedAdClosed(RewardedAd ad, bool finished);
-
-        /**
-        * Called when Rewarded Ad Complete (e.g - the video has been played to the end).
-        * You can use this event to initialize your reward
-        */
+        void onRewardedAdImpression(RewardedAd ad);
+        void onRewardedAdClicked(RewardedAd ad);
         void onRewardedAdRewarded(RewardedAd ad);
+        void onRewardedAdClosed(RewardedAd ad, bool finished);
+        void onRewardedAdExpired(RewardedAd ad);
     }
 }
