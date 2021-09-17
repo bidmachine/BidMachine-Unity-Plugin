9e9cb9065e4ebb53126b19d3251163a542db48c9
KulArtsiom <kulartsiom@gmail.com>
2021-09-17 11:45:40 +0300

    Update IInterstitialAdListener.cs

--- Assets/BidMachine/Common/IInterstitialAdListener.cs
+++ Assets/BidMachine/Common/IInterstitialAdListener.cs
@@ -6,49 +6,13 @@ namespace BidMachineAds.Unity.Common
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public interface IInterstitialAdListener
     {
-        /**
-        * Called when Ad was loaded and ready to be shown
-        */
         void onInterstitialAdLoaded(InterstitialAd ad);
-
-        /**
-        * Called when Ad load failed
-        */
         void onInterstitialAdLoadFailed(InterstitialAd ad, BMError error);
-
-        /**
-        * Called when Ad shown
-        */
         void onInterstitialAdShown(InterstitialAd ad);
-
-        /**
-        * Called when Ad Impression was tracked
-        */
-        void onInterstitialAdImpression(InterstitialAd ad);
-
-        /**
-        * Called when Ad was clicked
-        */
-        void onInterstitialAdClicked(InterstitialAd ad);
-
-        /**
-        * Called when Ad was clicked
-        */
-        void onInterstitialAdClosed(InterstitialAd ad);
-
-        /**
-        * Called when Ad expired
-        */
-        void onInterstitialAdExpired(InterstitialAd ad);
-
-        /**
-        * Called when ad show failed
-        */
         void onInterstitialAdShowFailed(InterstitialAd ad, BMError error);
-
-        /**
-        * Called when ad was closed (e.g - user click close button)
-        */
+        void onInterstitialAdImpression(InterstitialAd ad);
+        void onInterstitialAdClicked(InterstitialAd ad);
         void onInterstitialAdClosed(InterstitialAd ad, bool finished);
+        void onInterstitialAdExpired(InterstitialAd ad);
     }
 }
