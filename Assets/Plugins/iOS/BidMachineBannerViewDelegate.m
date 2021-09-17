4d03c9ab3926d2bd769a81ea6e12b2ccadd83f1c
KulArtsiom <kulartsiom@gmail.com>
2021-09-17 17:50:09 +0300

    Update BidMachineBannerViewDelegate.m

--- Assets/Plugins/iOS/BidMachineBannerViewDelegate.m
+++ Assets/Plugins/iOS/BidMachineBannerViewDelegate.m
@@ -2,21 +2,57 @@
 
 @implementation BidMachineBannerViewDelegate
 
+BDMBannerView *bdmBannerView;
+
 - (void)bannerView:(nonnull BDMBannerView *)bannerView failedWithError:(nonnull NSError *)error {
-    if(self.bannerViewLoadFailed){
-        self.bannerViewLoadFailed(bannerView, error);
+    if(self.onAdLoadFailed){
+        if (bannerView) {
+            bdmBannerView = bannerView;
+        }
+        self.onAdLoadFailed(bannerView, error);
     }
 }
 
 - (void)bannerViewReadyToPresent:(nonnull BDMBannerView *)bannerView {
-    if(self.bannerViewLoaded){
-        self.bannerViewLoaded(bannerView);
+    if(self.onAdLoaded){
+        if (bannerView) {
+            bdmBannerView = bannerView;
+        }
+        self.onAdLoaded(bannerView);
     }
 }
 
 - (void)bannerViewRecieveUserInteraction:(nonnull BDMBannerView *)bannerView {
-    if(self.bannerViewClicked){
-        self.bannerViewClicked(bannerView);
+    if(self.onAdClicked){
+        if (bannerView) {
+            bdmBannerView = bannerView;
+        }
+        self.onAdClicked(bannerView);
     }
 }
+
+- (void)bannerViewDidExpire:(BDMBannerView *)bannerView{
+    if(self.onAdExpired){
+        if (bannerView) {
+            bdmBannerView = bannerView;
+        }
+        self.onAdExpired(bannerView);
+    }
+}
+
+- (void)didProduceImpression:(nonnull id<BDMAdEventProducer>)producer{
+    if (self.onAdImpression) {
+        if (!bdmBannerView) {
+            bdmBannerView = [BDMBannerView new];
+        }
+        self.onAdImpression(bdmBannerView);
+    }
+}
+
+- (void)didProduceUserAction:(nonnull id<BDMAdEventProducer>)producer {
+    
+}
+
+
+
 @end
