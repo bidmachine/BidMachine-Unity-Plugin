717c414ddf4f2e00c17e454f7c24cbc9b73ccba5
KulArtsiom <kulartsiom@gmail.com>
2021-09-17 16:43:25 +0300

    Update BidMachineInterstitialDelegate.m

--- Assets/Plugins/iOS/BidMachineInterstitialDelegate.m
+++ Assets/Plugins/iOS/BidMachineInterstitialDelegate.m
@@ -2,40 +2,83 @@
 
 @implementation BidMachineInterstitialDelegate
 
+BDMInterstitial *bdmInterstitial;
+
+
 - (void)interstitial:(BDMInterstitial *)interstitial failedWithError:(NSError *)error {
-    if(self.interstitialLoadFailed){
-        self.interstitialLoadFailed(interstitial, error);
+    if(self.onAdLoadFailed){
+        if (!bdmInterstitial) {
+            bdmInterstitial = interstitial;
+        }
+        self.onAdLoadFailed(interstitial, error);
     }
 }
 
 - (void)interstitial:(BDMInterstitial *)interstitial failedToPresentWithError:(NSError *)error {
-    if(self.interstitialLoadFailed){
-        self.interstitialLoadFailed(interstitial, error);
+    if(self.onAdShowFailed){
+        if (!bdmInterstitial) {
+            bdmInterstitial = interstitial;
+        }
+        self.onAdShowFailed(interstitial, error);
     }
 }
 
 - (void)interstitialReadyToPresent:(nonnull BDMInterstitial *)interstitial {
-    if(self.interstitialLoaded){
-        self.interstitialLoaded(interstitial);
+    if(self.onAdLoaded){
+        if (!bdmInterstitial) {
+            bdmInterstitial = interstitial;
+        }
+        self.onAdLoaded(interstitial);
     }
 }
 
 - (void)interstitialDidDismiss:(nonnull BDMInterstitial *)interstitial {
-    if(self.interstitialClosed){
-        self.interstitialClosed(interstitial);
+    if(self.onAdClosed){
+        if (!bdmInterstitial) {
+            bdmInterstitial = interstitial;
+        }
+        self.onAdClosed(interstitial, true);
     }
 }
 
 - (void)interstitialRecieveUserInteraction:(nonnull BDMInterstitial *)interstitial {
-    if(self.interstitialClicked){
-        self.interstitialClicked(interstitial);
+    if(self.onAdClicked){
+        if (!bdmInterstitial) {
+            bdmInterstitial = interstitial;
+        }
+        self.onAdClicked(interstitial);
     }
 }
 
 - (void)interstitialWillPresent:(nonnull BDMInterstitial *)interstitial {
-    if(self.interstitialShown){
-        self.interstitialShown(interstitial);
+    if(self.onAdShown){
+        if (!bdmInterstitial) {
+            bdmInterstitial = interstitial;
+        }
+        self.onAdShown(interstitial);
     }
+}
+
+- (void)interstitialDidExpire:(BDMInterstitial *)interstitial{
+    if(self.onAdExpired){
+        if (!bdmInterstitial) {
+            bdmInterstitial = interstitial;
+        }
+        self.onAdExpired(interstitial);
+    }
+}
+
+- (void)didProduceImpression:(nonnull id<BDMAdEventProducer>)producer{
+    if (self.onAdImpression) {
+        if (!bdmInterstitial) {
+            bdmInterstitial = [BDMInterstitial new];
+        }
+        self.onAdImpression(bdmInterstitial);
+    }
+}
+
+- (void)didProduceUserAction:(nonnull id<BDMAdEventProducer>)producer {
+    
 }
 
 @end
