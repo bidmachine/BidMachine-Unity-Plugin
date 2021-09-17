2e9d88d3673c07378e65d9c59fc2374914dcd2e6
KulArtsiom <kulartsiom@gmail.com>
2021-09-17 12:21:56 +0300

    Update IBannerViewListener.cs

--- Assets/BidMachine/Common/IBannerViewListener.cs
+++ Assets/BidMachine/Common/IBannerViewListener.cs
@@ -7,34 +7,11 @@ namespace BidMachineAds.Unity.Common
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public interface IBannerListener
     {
-        /**
-        * Called when Ad was loaded and ready to be shown
-        */
         void onBannerAdLoaded(BannerView ad);
-
-        /**
-        * Called when Ad load failed
-        */
         void onBannerAdLoadFailed(BannerView ad, BMError error);
-
-        /**
-        * Called when Ad shown
-        */
         void onBannerAdShown(BannerView ad);
-
-        /**
-        * Called when Ad Impression was tracked
-        */
         void onBannerAdImpression(BannerView ad);
-
-        /**
-        * Called when Ad was clicked
-        */
         void onBannerAdClicked(BannerView ad);
-
-        /**
-        * Called when Ad expired
-        */
         void onBannerAdExpired(BannerView ad);
     }
 }
