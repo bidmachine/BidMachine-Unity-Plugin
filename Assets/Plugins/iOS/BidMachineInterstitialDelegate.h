f842050c8b21bb594daa7209a2f8112267f6fdb5
KulArtsiom <kulartsiom@gmail.com>
2021-09-20 14:36:03 +0300

    Update BidMachineInterstitialDelegate.h

--- Assets/Plugins/iOS/BidMachineInterstitialDelegate.h
+++ Assets/Plugins/iOS/BidMachineInterstitialDelegate.h
@@ -1,19 +1,19 @@
 #import <Foundation/Foundation.h>
 #import <BidMachine/BidMachine.h>
 
-typedef void (*BidMachineInterstitialCallbacks) (BDMInterstitial *interstitial);
+typedef void (*BidMachineInterstitialCallback) (BDMInterstitial *interstitial);
 typedef void (*BidMachineInterstitialFailedCallback) (BDMInterstitial *interstitial, NSError *error);
 typedef void (*BidMachineInterstitialClosedCallback) (BDMInterstitial *interstitial, bool finished);
 
 @interface BidMachineInterstitialDelegate: NSObject <BDMInterstitialDelegate, BDMAdEventProducerDelegate>
 
-@property (assign, nonatomic) BidMachineInterstitialCallbacks onAdLoaded;
+@property (assign, nonatomic) BidMachineInterstitialCallback onAdLoaded;
 @property (assign, nonatomic) BidMachineInterstitialFailedCallback onAdLoadFailed;
-@property (assign, nonatomic) BidMachineInterstitialCallbacks onAdShown;
+@property (assign, nonatomic) BidMachineInterstitialCallback onAdShown;
 @property (assign, nonatomic) BidMachineInterstitialFailedCallback onAdShowFailed;
-@property (assign, nonatomic) BidMachineInterstitialCallbacks onAdImpression;
-@property (assign, nonatomic) BidMachineInterstitialCallbacks onAdClicked;
+@property (assign, nonatomic) BidMachineInterstitialCallback onAdImpression;
+@property (assign, nonatomic) BidMachineInterstitialCallback onAdClicked;
 @property (assign, nonatomic) BidMachineInterstitialClosedCallback onAdClosed;
-@property (assign, nonatomic) BidMachineInterstitialCallbacks onAdExpired;
+@property (assign, nonatomic) BidMachineInterstitialCallback onAdExpired;
 
 @end
