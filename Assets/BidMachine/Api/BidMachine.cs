0714ef7dc27b9ed53655880e9e6a68d6d3435db9
KulArtsiom <kulartsiom@gmail.com>
2021-11-01 11:19:04 +0300

    Update BidMachine.cs

--- Assets/BidMachine/Api/BidMachine.cs
+++ Assets/BidMachine/Api/BidMachine.cs
@@ -25,7 +25,7 @@ namespace BidMachineAds.Unity.Api
             return client ?? (client = BidMachineClientFactory.GetBidMachine());
         }
 
-        public static string BIDMACHINE_UNITY_PLUGIN_VERSION = "1.0.2";
+        public static string BIDMACHINE_UNITY_PLUGIN_VERSION = "1.0.3";
 
         /// <summary>
         /// Initializes BidMachine SDK.
