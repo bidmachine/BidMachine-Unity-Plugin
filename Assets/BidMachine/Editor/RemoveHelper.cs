48cc5c2e6d95d99feccce8954e899a6d3b6857a3
Mike <57871929+maninbox1@users.noreply.github.com>
2022-08-04 11:35:20 +0300

    Update RemoveHelper.cs
    some fixes

--- Assets/BidMachine/Editor/RemoveHelper.cs
+++ Assets/BidMachine/Editor/RemoveHelper.cs
@@ -1,9 +1,9 @@
-﻿#pragma warning disable 0649
+#pragma warning disable 0649
 using System.IO;
 using UnityEditor;
 using System.Text.RegularExpressions;
 using UnityEngine;
-// using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 // using System.Linq;
 
@@ -28,19 +28,30 @@ namespace BidMachine.Unity.Editor.Utils
                 "Yes",
                 "Cancel"))
             {
-                
-                   FileUtil.DeleteFileOrDirectory(Path.Combine(Application.dataPath, "Plugins/Android"));
-                    FileUtil.DeleteFileOrDirectory(Path.Combine(Application.dataPath, "Plugins/iOS"));
                     FileUtil.DeleteFileOrDirectory(Path.Combine(Application.dataPath, "BidMachine"));
-
-                    FileUtil.DeleteFileOrDirectory(Path.Combine(Application.dataPath, "Plugins/Android" + ".meta"));
-                    FileUtil.DeleteFileOrDirectory(Path.Combine(Application.dataPath, "Plugins/iOS" + ".meta"));
                     FileUtil.DeleteFileOrDirectory(Path.Combine(Application.dataPath, "BidMachine" + ".meta"));
+                    
+                    
+                    new List<string>(Directory.GetFiles("Assets/Plugins/iOS")).ForEach(file => {
+                    Regex re = new Regex("bidmachine", RegexOptions.IgnoreCase);
+                    if (re.IsMatch(file))
+                        File.Delete(file);
+                        File.Delete(file + ".meta");
+                    });
+                    new List<string>(Directory.GetFiles("Assets/Plugins/Android")).ForEach(file => {
+                    Regex re = new Regex("bidmachine", RegexOptions.IgnoreCase);
+                    if (re.IsMatch(file))
+                        File.Delete(file);
+                        File.Delete(file + ".meta");
+                    });
+                    
+                    Directory.Delete("Assets/Plugins/Android/bidmachine.androidlib", true);
+            }
                  
                 AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
                 
-            }
         }
 
     }
+
 }
