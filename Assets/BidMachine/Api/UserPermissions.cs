1f1324a42da2c1fcbeb44f70e96e6f04b0168317
Andrei Pugachev <andrei.pugachev@appodeal.com>
2024-12-23 11:09:21 +0300

    APDX-11607: Remove missing permissions

--- Assets/BidMachine/Api/UserPermissions.cs
+++ Assets/BidMachine/Api/UserPermissions.cs
@@ -3,37 +3,33 @@ using UnityEngine;
 
 namespace BidMachineAds.Unity.Api
 {
+    [System.Obsolete("This class is deprecated and will be removed in future versions")]
     public sealed class UserPermissions : IUserPermissions
     {
-        private readonly IUserPermissions client;
+        public UserPermissions() { }
 
-        public UserPermissions()
-        {
-            this.client = BidMachineClientFactory.GetUserPermissions();
-        }
-
-        public UserPermissions(IUserPermissions client)
-        {
-            this.client = client;
-        }
+        public UserPermissions(IUserPermissions client) { }
 
         /// <summary>
         /// Check CoarseLocation and FineLocation permission.
         /// See <see cref="BidMachine.CheckAndroidPermissions"/> for resulting triggered event.
         /// <param name="permission">Permission.CoarseLocation or Permission.CoarseLocation</param>
         /// </summary>
+        [System.Obsolete("Check method is deprecated and will be removed in future versions")]
         public bool Check(string permission)
         {
-            return client.Check(permission);
+            Debug.LogWarning("UserPermissions.Check is deprecated");
+            return false;
         }
 
         /// <summary>
         /// Request CoarseLocation and FineLocation permissions.
         /// See <see cref="BidMachine.RequestAndroidPermissions"/> for resulting triggered event.
         /// </summary>
+        [System.Obsolete("Request method is deprecated and will be removed in future versions")]
         public void Request()
         {
-            client.Request();
+            Debug.LogWarning("UserPermissions.Request is deprecated");
         }
     }
 }
--- Assets/BidMachine/Common/IUserPermissions.cs
+++ Assets/BidMachine/Common/IUserPermissions.cs
@@ -5,12 +5,5 @@
         bool Check(string permission);
 
         void Request();
-
-        public interface IListener
-        {
-            void WriteExternalStorageResponse(int result);
-
-            void AccessCoarseLocationResponse(int result);
-        }
     }
 }
