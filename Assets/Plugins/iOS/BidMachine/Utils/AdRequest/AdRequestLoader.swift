49e5bcff0d3be0641f97b33c37ffd011e093ec8f
Dzmitry Paplauski <dzmitry@MacBook-Pro-Dzmitry.local>
2025-05-28 22:10:28 +0200

    build auction request with updated API

--- Assets/Plugins/iOS/BidMachine/Utils/AdRequest/AdRequestLoader.swift
+++ Assets/Plugins/iOS/BidMachine/Utils/AdRequest/AdRequestLoader.swift
@@ -11,7 +11,7 @@ import BidMachine
 final class AdRequestLoader<T: BidMachineAdProtocol> {
     enum RequestError: Error {
         case noAd
-        case unableToGetConfig
+        case unableToGetPlacement
         case underlying(Error)
         case unableToCastToProvidedType
     }
@@ -28,42 +28,47 @@ final class AdRequestLoader<T: BidMachineAdProtocol> {
         request: AdRequest,
         callback: @escaping (Result<Ad, RequestError>) -> Void
     ) {
-        guard let config = self.createConfiguration(for: request) else {
-            callback(.failure(.unableToGetConfig))
-            return
-        }
-        bidMachine.ad(config) { ad, error in
-            if let error {
-                callback(.failure(.underlying(error)))
-                return
+        do {
+            let adRequest = try createAuctionRequest(from: request)
+            bidMachine.ad(request: adRequest) { ad, error in
+                if let error {
+                    callback(.failure(.underlying(error)))
+                    return
+                }
+                guard let ad else {
+                    callback(.failure(.noAd))
+                    return
+                }
+                guard let casted = ad as? Ad else {
+                    callback(.failure(.unableToCastToProvidedType))
+                    return
+                }
+                callback(.success(casted))
             }
-            guard let ad else {
-                callback(.failure(.noAd))
-                return
-            }
-            guard let casted = ad as? Ad else {
-                callback(.failure(.unableToCastToProvidedType))
-                return
-            }
-            callback(.success(casted))
+        } catch {
+            callback(.failure(error))
         }
     }
 
-    private func createConfiguration(for adRequest: AdRequest) -> BidMachineRequestConfigurationProtocol? {
-        guard let config = try? bidMachine.requestConfiguration(adRequest.format) else {
-            return nil
-        }
-        config.populate { builder in
-            builder.withUnitConfigurations(adRequest.configurations)
+    private func createAuctionRequest(from adRequest: AdRequest) throws (RequestError) -> BidMachineAuctionRequest {
+        // FIXME: where is timeout setting now?
+        
+        let placement = try? bidMachine.placement(from: adRequest.format) { builder in
+            adRequest.placementId.apply { builder.withPlacementId($0) }
             builder.withCustomParameters(adRequest.customParams)
-
+        }
+        
+        guard let placement else {
+            throw RequestError.unableToGetPlacement
+        }
+        
+        let request = bidMachine.auctionRequest(placement: placement) { builder in
             adRequest.priceFloors.forEach {
                 builder.appendPriceFloor($0.value, $0.key)
             }
             adRequest.payload.apply { builder.withPayload($0) }
-            adRequest.placementId.apply { builder.withPlacementId($0) }
-            adRequest.timeout.apply { builder.withTimeout($0) }
+            builder.withUnitConfigurations(adRequest.configurations)
         }
-        return config
+        return request
     }
 }
